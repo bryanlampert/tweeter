cbebccfb16e2f0b53e626da7a2c54822a4e92e01
bryanlampert <bryanlampert@gmail.com>
2017-12-14 21:25:55 +0000

    Added external database, updated some css and fixed some minor user experience bugs in functionality

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,16 +1,17 @@
-/*
- * Client-side JS logic goes here
- * jQuery is already loaded
- * Reminder: Use (and do all your DOM work in) jQuery's document ready function
- */
+// Wait until the document is loaded before executing the code
 $(document).ready(function() {
 
+  // Compose New Tweet button funtionality in the Nav bar
+  // Whel clicked the compose new tweet form slides to show or hide.
+  // After it is opened again the cursor starts in the text box
   $('.composeButton').click(function(){
     $('.new-tweet').slideToggle("slow", function() {
       $('textarea').select();
+      $('[name="blank-text"]').remove();
     });
   });
 
+  // Function to render all of the tweets on the page
   function renderTweets(tweets) {
     tweets.forEach(function(tweet) {
       $tweet = createTweetElement(tweet);
@@ -18,15 +19,44 @@ $(document).ready(function() {
     });
   }
 
+  // Function that creates a new tweet post at the top of the list of tweets
   function createTweetElement(tweet) {
 
     //Checks to make sure the inputted tweet is safe
     function escape(str) {
-      var div = document.createElement('div');
+      let div = document.createElement('div');
       div.appendChild(document.createTextNode(str));
       return div.innerHTML;
     }
 
+    //Calculates the date timestamp in the individual tweet footer
+    function timeSince(date) {
+      let seconds = Math.floor((new Date() - date) / 1000);
+      let interval = Math.floor(seconds / 31536000);
+
+      if (interval > 1) {
+        return interval + " years";
+      }
+      interval = Math.floor(seconds / 2592000);
+      if (interval > 1) {
+        return interval + " months";
+      }
+      interval = Math.floor(seconds / 86400);
+      if (interval > 1) {
+        return interval + " days";
+      }
+      interval = Math.floor(seconds / 3600);
+      if (interval > 1) {
+        return interval + " hours";
+      }
+      interval = Math.floor(seconds / 60);
+      if (interval > 1) {
+        return interval + " minutes";
+      }
+      return Math.floor(seconds) + " seconds";
+    }
+
+    // Function returns a new tweet in the following html format
     return (
       `<article class="tweet">
           <header class="tweet-header">
@@ -41,7 +71,7 @@ $(document).ready(function() {
             ${escape(tweet.content.text)}
           </section>
           <footer class="tweet-footer">
-            <span>${tweet.created_at} days ago</span>
+            <span>${timeSince(new Date(tweet.created_at))} ago</span>
             <img class="footer-images" src="/images/heart.png">
             <img class="footer-images" src="/images/retweeter.png">
             <img class="footer-images" src="/images/flag.png">
@@ -50,6 +80,7 @@ $(document).ready(function() {
     );
   }
 
+  // Function for loading the tweets from the database
   function loadTweets() {
     $.ajax({
       url: '/tweets',
@@ -62,6 +93,9 @@ $(document).ready(function() {
 
   }
 
+  // Functino for posting new tweets to the database.
+  // Serializes the data and on a successfull call posts the tweet to the top
+  //    of the tweet list page
   function postTweets() {
     let $tweetData = $('textarea').serialize();
     let $text = ($tweetData).replace('text=', '');
@@ -76,6 +110,7 @@ $(document).ready(function() {
           $('form').each(function(){
               this.reset();
           });
+          $('main').select();
           let counter = $('.counter')[0];
           counter.innerHTML = 140;
         });
@@ -85,22 +120,28 @@ $(document).ready(function() {
 
   }
 
+  // When the submit button is clicked in the compose tweet form
+  // Has two checks for errors in inputs. --> For trying to submit a blank field and
+  //    if the post has more than 140 characters.
+  // If neither of the above conditions are true, then it calls the postTweets function
   $('form').submit(function(event) {
     event.preventDefault();
-    let $text = ($('textarea').serialize()).replace('text=', '');
-    let $removeErrMsg = $('.Tweet-error').remove();
-    if ($text === '' || $text === null) {
+    const text = ($('textarea').serialize()).replace('text=', '');
+    const $removeErrMsg = $('.Tweet-error').remove();
+    const textLength = $('textarea').val();
+    if (text === '' || text === null) {
       $removeErrMsg;
-      $(this).append(`<p class="Tweet-error" style="color: red; text-align: center;">Please enter text!</p>`);
-    } else if ($text.length > 140 ) {
+      $(this).append(`<p class="Tweet-error" name="blank-text" style="color: red; text-align: center;">You forgot to tweet something!</p>`);
+    } else if (textLength.length > 140 ) {
       $removeErrMsg;
-      $(this).append(`<p class="Tweet-error" style="color: red; text-align: center;">Tweet is too long! ..sorry</p>`);
+      $(this).append(`<p class="Tweet-error" name="text-too-long" style="color: red; text-align: center;">Tweet is too long! ..sorry</p>`);
     } else {
       $removeErrMsg;
       postTweets();
     }
   });
 
+  // Calls the loadTweets function
   loadTweets();
 
 });
--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -2,21 +2,37 @@
 
 const charMax = 140;
 
+// Function for the character counter on the compose new tweet form
 function char() {
   let inputText = $(this).val().length;
   let counter = $(this).parent().find(".counter")[0];
+  // Calculates the amount of characters remaining
   let charRemaining = charMax - inputText;
+  // Sets the counter on the page to the amount of characters remaining
   counter.innerText = charRemaining;
+
+  // Checks if the character count is more than 140, if so adds a class for css styling
   if (charRemaining < 0) {
     $(counter).addClass("negative-char-count");
   } else {
     $(counter).removeClass("negative-char-count");
   }
+
+  // Checks if the blank text error message is true. Removes this message once another key is typed
+  if ($('[name="blank-text"]')) {
+    $('[name="blank-text"]').remove();
+  }
+  // Checks if the character counter is in the negative. If it is positive, removes the text too long error message
+  if (charRemaining >= 0) {
+    $('[name="text-too-long"]').remove();
+  }
+
 }
 
-
+// When the document is ready performs the below code
 $(document).ready(function() {
 
+  // On input in the compose new tweet text area, calls the char function for the character counting
   $(".new-tweet textarea").on("input", char);
 
 });
